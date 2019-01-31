9934337a8f4cf877a09479572a808ceaa22abeb2
Matt Debont <matt.debont@jncc.gov.uk>
2019-01-31 12:53:00 +0000

    Basic stab at a html parser for the website content,

--- src/main/java/search/ingester/Ingester.java
+++ src/main/java/search/ingester/Ingester.java
@@ -18,6 +18,8 @@ import org.apache.http.HttpRequestInterceptor;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.delete.DeleteResponse;
@@ -117,6 +119,16 @@ public class Ingester implements RequestHandler<SQSEvent, Void> {
                 }**/
                 try {
                     document = parseFile(document);
+                } catch (Exception err) {
+                    throw new RuntimeException(err);
+                }
+            } else {
+                try {
+                    // Try to detect HTML?
+                    //      Attempt HTML extraction?
+                    document = parseHTMLContentString(document);
+                    // Try to detect JSON?
+                    //      Attempt JSON extraction?
                 } catch (Exception err) {
                     throw new RuntimeException(err);
                 }
@@ -286,6 +298,38 @@ public class Ingester implements RequestHandler<SQSEvent, Void> {
 
         // Clear b64 encoded file
         document.setFileBase64(null);
+
+        return document;
+    }
+
+    private Document parseHTMLContentString(Document document) throws IOException, SAXException, TikaException {
+        BodyContentHandler handler = new BodyContentHandler(TIKA_MAX_CHARACTER_LIMIT);
+        HtmlParser parser = new HtmlParser();
+        Metadata metadata = new Metadata();
+
+        InputStream stream = new ByteArrayInputStream(document.getContent().getBytes());
+
+        try {
+            parser.parse(stream, handler, metadata, new ParseContext());
+        } catch(SAXException ex) {
+            if (ex.getClass().getCanonicalName() != "org.apache.tika.sax.WriteOutContentHandler$WriteLimitReachedException") {
+                throw ex;
+            } else {
+                System.out.println(String.format("Got more characters than current Tika limit (%d), truncating to limit", TIKA_MAX_CHARACTER_LIMIT));
+            }
+        }
+
+        // Grab the extracted content from the parser and strip out all repeated whitespace characters as we don't need
+        // them, if no content don't replace the existing content
+        String newContent = handler.toString().replaceAll("\\s+", " ").trim();
+        if (!newContent.isEmpty()) {
+            document.setContent(newContent);
+        }
+
+        // If a title exists in the document metadata replace the document title with it
+        if (metadata.get("title") != null) {
+            document.setTitle(metadata.get("title"));
+        }
 
         return document;
     }
