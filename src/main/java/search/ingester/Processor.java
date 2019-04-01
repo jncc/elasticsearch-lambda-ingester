78b5f9a8e7fb2aea4a62b02a2babee5a11dce686
Matt Debont <matt.debont@jncc.gov.uk>
2019-04-01 10:15:04 +0100

    Removed some uneeded imports

--- src/main/java/search/ingester/Processor.java
+++ src/main/java/search/ingester/Processor.java
@@ -1,8 +1,6 @@
 package search.ingester;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
